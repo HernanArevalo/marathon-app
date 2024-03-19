7c3eaea835ad1ea9e436ccb888321af7c71c6a1e
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-03-18 22:16:54 -0300

    Start the file update system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,50 @@
+'use client';
 import { Counter, FinisherForm, ResultsList, FileCharge } from '@/components';
-import {} from '@/components/file-charge/FileCharge';
-
-export const metadata = {
-  title: 'Marathon | Race',
-  description: 'Marathon | Race',
-};
+import { useEffect, useState } from 'react';
 
 export default function HomePage() {
+  const [useFile, setUseFile] = useState(true);
+  const [fileLoaded, setFileLoaded] = useState(false);
+  const [file, setFile] = useState([]);
+
+  useEffect(() => {
+    console.log(file);
+  }, [file]);
+
   return (
-    <div className='flex flex-row min-h-[100vh]'>
-      <div className='relative w-[50%] min-h-[100vh] flex flex-col justify-center items-center p-5 gap-10'>
-        <FileCharge />
-        <Counter />
-        <FinisherForm />
+    <div className='flex flex-col min-h-[100vh] md:flex-row'>
+      <div className='relative md:w-[50%] min-h-[100vh] flex flex-col justify-center items-center p-5 gap-10'>
+        { useFile && !fileLoaded?
+          <FileCharge
+          useFile={useFile}
+          setUseFile={setUseFile}
+          fileLoaded={fileLoaded}
+          setFileLoaded={setFileLoaded}
+          file={file}
+          setFile={setFile}
+        />
+        :
+        <>
+          <button className="absolute top-10 left-10 bg-gray-500 text-white p-2 rounded-md cursor-pointer transition-all 
+                        hover:bg-gray-200 hover:text-black"
+                  onClick={()=>{setFileLoaded(false)}}>
+            Cargar otra tabla            
+          </button>
+          <Counter />
+          {fileLoaded ? (
+            <FinisherForm />
+          ) : (
+            <button
+              onClick={() => {
+                setUseFile(true);
+              }}
+              className='bg-gray-500 text-white p-2 rounded-md cursor-pointer transition-all 
+                              hover:bg-gray-200 hover:text-black'
+            >
+              Cargar planilla de Excel
+            </button>
+          )}
+        </>}
       </div>
       <div className='w-[50%] min-h-[100vh] flex flex-col justify-center items-center p-5 gap-10'>
         <ResultsList />
--- src/components/file-charge/FileCharge.tsx
+++ src/components/file-charge/FileCharge.tsx
@@ -1,28 +1,70 @@
-'use client';
+"use client"
 import clsx from 'clsx';
-import { useState } from 'react';
 import { Divider } from './components';
+import * as XLSX from 'xlsx';
 
-export const FileCharge = () => {
-  const [useFile, setUseFile] = useState(true);
-  const [fileLoaded, setFileLoaded] = useState(false);
+interface Props {
+  useFile: boolean,
+  setUseFile: Function,
+  fileLoaded: boolean,
+  setFileLoaded: Function,
+  file: Array<{}>,
+  setFile: Function,
+}
+
+export const FileCharge = ({
+  useFile,
+  setUseFile,
+  fileLoaded,
+  setFileLoaded,
+  file,
+  setFile
+}: Props) => {
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files || files.length === 0) return;
+
+    const reader = new FileReader();
+    reader.readAsBinaryString(files[0]);
+    reader.onload = (e) => {
+      const data = e.target?.result;
+      const workbook = XLSX.read(data, { type: 'binary' });
+      
+      setFile([])
+      for (const i in workbook.SheetNames) {
+        const sheetName = workbook.SheetNames[i];
+        const sheet = workbook.Sheets[sheetName];
+        const parsedData = XLSX.utils.sheet_to_json(sheet);
+
+        if (parsedData) {
+          setFile((prevData:Array<{}>) => [
+            ...prevData,
+            { name: sheetName, data: parsedData },
+          ]);
+          setFileLoaded(true);
+          setUseFile(true)
+        }
+      }
+    };
+  };
 
   return (
     <div
-      className={clsx(
-        useFile ? 'inline' : 'hidden',
+      className={clsx( useFile && !fileLoaded? 'inline':'hidden',
         'text-2xl text-center bg-blue-800 text-white absolute w-[90%] h-[90%] flex flex-col justify-center items-center rounded p-10'
       )}
     >
       <div className='h-full flex flex-col gap-10 justify-center items-center'>
         <p>Selecciona un archivo Excel para cargar los participantes!</p>
         <form>
-          <label className='block'>
+          <label className='block' >
             <input
+              onChange={handleFileUpload} 
               type='file'
               accept='.xlsx, .xls'
               className='text-xl text-white
-                file:me-4 file:py-2 file:px-4
+                file:me-4 file:py-2 file:px-4 file:transition-all
                 file:rounded-lg file:border-0
                 file:text-sm file:font-semibold
                 file:bg-blue-600 file:text-blue-900
@@ -37,7 +79,7 @@ export const FileCharge = () => {
       </div>
       <Divider />
       <div className='w-full h-full flex justify-center items-center'>
-        <p>Usar solo cronómetro</p>
+        <button className="text-blue-900 bg-blue-200 p-4 rounded-md transition-all hover:bg-blue-400 cursor-pointer" onClick={() =>{setUseFile(false)}}>Usar sólo cronómetro</button>
       </div>
     </div>
   );
