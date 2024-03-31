d399ff496126a1db809f48718e3401c9239af38e
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-03-31 13:43:16 -0300

    use the store data in the counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,12 +1,14 @@
-"use client"
-import { useRef, useState } from 'react';
+'use client';
+import { useEffect, useRef, useState } from 'react';
 import { orbitron } from '@/lib';
 import { PauseButton, StopButton } from '@/components';
-
+import { useStore } from '@/store';
 
 export const Counter = () => {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  const { time, startTime, setTime } = useStore();
+  const { isRunning, setIsRunning } = useStore();
+  const [isLoaded, setIsLoaded] = useState(false);
+
   const intervalRef = useRef<any>(null);
 
   const toggleTimer = () => {
@@ -29,7 +31,25 @@ export const Counter = () => {
     setIsRunning(false);
   };
 
-  const formatTime = (milliseconds: number) => {
+  useEffect(() => {
+    if (isRunning && !intervalRef.current) {
+      intervalRef.current = setInterval(() => {
+        setTime(Date.now() - startTime);
+      }, 10);
+    }
+  
+    setIsLoaded(true)
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
+
+  function formatTime(milliseconds: number) {
     const hours = Math.floor(milliseconds / 3600000)
       .toString()
       .padStart(2, '0');
@@ -41,23 +61,29 @@ export const Counter = () => {
       .padStart(2, '0');
     const millisecondsPart = (milliseconds % 1000).toString().padStart(3, '0');
     return { hours, minutes, seconds, millisecondsPart };
-  };
+  }
 
   return (
     <>
       <div className='flex flex-col w-full gap-3'>
-        <h1 className='text-3xl md:text-5xl font-bold w-full text-left'>TIMER</h1>
+        <h1 className='text-3xl md:text-5xl font-bold w-full text-left'>
+          TIMER
+        </h1>
         <div
-          className={`${orbitron.className} 
-                      font-bold text-4xl 
-                      md:font-bold md:text-6xl
-                      text-center bg-white text-black w-full p-3 flex flex-row justify-center`}
+          className={`${orbitron.className} md:font-bold md:text-6xl
+                      font-bold text-4xl text-center bg-white text-black w-full p-3 flex flex-row justify-center`}
         >
-          <span className='min-w-[70px] md:min-w-[101px]'>{formatTime(time).hours}</span>
+          <span className='min-w-[70px] md:min-w-[101px]'>
+            {formatTime(time).hours}
+          </span>
           <span>:</span>
-          <span className='min-w-[70px] md:min-w-[101px]'>{formatTime(time).minutes}</span>
+          <span className='min-w-[70px] md:min-w-[101px]'>
+            {formatTime(time).minutes}
+          </span>
           <span>:</span>
-          <span className='min-w-[70px] md:min-w-[101px]'>{formatTime(time).seconds}</span>
+          <span className='min-w-[70px] md:min-w-[101px]'>
+            {formatTime(time).seconds}
+          </span>
           <span>.</span>
           <div className='min-w-[89px] md:w-[151px]'>
             {formatTime(time).millisecondsPart}
@@ -65,7 +91,7 @@ export const Counter = () => {
         </div>
       </div>
       <div className='flex flex-row gap-10 text-4xl md:text-5xl'>
-        <PauseButton toggleTimer={toggleTimer} isRunning={isRunning} />
+        <PauseButton toggleTimer={toggleTimer}/>
         <StopButton resetTimer={resetTimer} />
       </div>
     </>
