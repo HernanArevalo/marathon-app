a0dcd45cd7ceed6d6fa001a4bdbcae8b6fa65ea5
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-10 13:19:18 -0300

    change button text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function HomePage() {
                 setPlayers([]);
               }}
             >
-              Eliminar la tabla
+              Eliminar tabla
             </button>
           </div>
         )}
