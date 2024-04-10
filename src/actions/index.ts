a987112883f81b0a28e932dfb09574bd5bfce2dc
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-10 13:48:21 -0300

    set saveExcelFile

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,1 +1,1 @@
-export * from './saveFile'
+export * from './saveExcelFile'
---
+++ src/actions/saveExcelFile.ts
@@ -0,0 +1,22 @@
+import { formatTime, generateFileName } from '@/helpers';
+import { Finisher } from '@/interfaces';
+import * as XLSX from 'xlsx';
+
+export const saveExcelFile = (finishers:Finisher[]) => {
+
+  const dataSetted = finishers.map((finisher, idx) => {
+    return {
+      pos: idx+1,
+      ...finisher,
+      TIEMPO: formatTime(finisher.time).general
+    }
+  })
+
+  const ws = XLSX.utils.json_to_sheet(dataSetted);
+
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb,ws, 'Resultados')
+
+
+  XLSX.writeFile(wb, generateFileName() );
+}
