4ee32a009c8dc21127a1409393bfdf0a39d7dc58
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-10 19:30:43 -0300

    format file

--- src/components/finisher-form/FinisherForm.tsx
+++ src/components/finisher-form/FinisherForm.tsx
@@ -25,22 +25,21 @@ export const FinisherForm = () => {
     event.preventDefault();
 
     if (playersFiltered.length > 0) {
-      const player0 = players[0]
+      const player0 = players[0];
 
       addFinisher(playersFiltered[0]);
     } else {
-      const defaultFinisher:Player = {
-        ...players[0] // Copiar todas las claves y valores del objeto original
+      const defaultFinisher: Player = {
+        ...players[0],
       };
-      defaultFinisher[identifier] = inputValue // Asignar un nuevo valor al id (en este caso "nuevoID")
-      
-      // Asignar cadenas vacías a todas las claves, excepto a "id"
+      defaultFinisher[identifier] = inputValue;
+
       Object.keys(defaultFinisher).forEach((key) => {
         if (key !== identifier) {
           defaultFinisher[key] = '';
         }
       });
-      addFinisher(defaultFinisher)
+      addFinisher(defaultFinisher);
     }
     setInputValue('');
     setPlayersFiltered([]);
@@ -63,29 +62,29 @@ export const FinisherForm = () => {
   }, [notFinishers]);
 
   return (
-    <form className='w-full max-w-sm' onSubmit={handleSubmit}>
-      <div className='md:flex md:items-center mb-6 gap-4'>
-        <div className='md:w-1/3'>
-          <label className='block text-white font-bold text-3xl md:text-right mb-1 md:mb-0 pr-4'>
+    <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+      <div className="md:flex md:items-center mb-6 gap-4">
+        <div className="md:w-1/3">
+          <label className="block text-white font-bold text-3xl md:text-right mb-1 md:mb-0 pr-4">
             {identifier.toUpperCase()}
           </label>
         </div>
-        <div className='md:w-2/3'>
-          <div className='relative'>
+        <div className="md:w-2/3">
+          <div className="relative">
             <input
-              className='text-xl bg-gray-200 appearance-none border-2 border-gray-200 rounded w-36 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
-              id='inline-full-name'
-              type='text'
-              placeholder='123'
+              className="text-xl bg-gray-200 appearance-none border-2 border-gray-200 rounded w-36 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="text"
+              placeholder="123"
               value={inputValue}
               onChange={handleInputChange}
             />
             {playersFiltered.length > 0 && inputValue !== '' && (
-              <div className='absolute text-black bg-white border border-gray-300 rounded mt-1 w-32 z-10'>
+              <div className="absolute text-black bg-white border border-gray-300 rounded mt-1 w-32 z-10">
                 {playersFiltered.map((player, index) => (
                   <div
                     key={index}
-                    className='px-3 py-2 hover:bg-gray-100 cursor-pointer'
+                    className="px-3 py-2 hover:bg-gray-100 cursor-pointer"
                     onClick={() => handleOptionClick(player)}
                   >
                     {player[identifier as keyof typeof player]}
@@ -96,15 +95,15 @@ export const FinisherForm = () => {
           </div>
         </div>
       </div>
-      <div className='md:flex md:items-center'>
-        <div className='md:w-1/3'></div>
-        <div className='md:w-2/3'>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
           <button
             className={clsx(
               inputValue !== '' && 'bg-green-700',
               'shadow bg-gray-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded transition-all'
             )}
-            type='submit'
+            type="submit"
           >
             CARGAR
           </button>
