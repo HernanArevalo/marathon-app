8013061a196bf2d661fc55018872483f62e92c0d
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-10 14:19:07 -0300

    add global interfaces and format it

--- src/store/marathon.store.ts
+++ src/store/marathon.store.ts
@@ -1,27 +1,25 @@
 import { sortByTime, sortByIdentifier } from '@/helpers';
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { Finisher, Player } from '@/interfaces';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-interface State{
-  time: any,
-  startTime: any,
-  isRunning: Boolean,
-  players: {}[],
-  notFinishers: {}[],
-  finishers: {}[],
-  identifier: string,
-  
-  addFinisher: (player: Object) => void;
+interface State {
+  time: any;
+  startTime: any;
+  isRunning: Boolean;
+  players: Player[];
+  notFinishers: Player[];
+  finishers: Finisher[];
+  identifier: string;
+
+  addFinisher: (player: Player) => void;
   setIdentifier: (id: string) => void;
-  setPlayers: (players: {}[]) => void;
-  setTime: (time: Date|number) => void;
-  setIsRunning: (bool:Boolean) => void;
+  setPlayers: (players: Player[]) => void;
+  setTime: (time: Date | number) => void;
+  setIsRunning: (bool: Boolean) => void;
   resetTable: () => void;
-  deleteFinisher: (player: Object) => void;
-  
+  deleteFinisher: (player: Finisher) => void;
 }
-
-
 
 export const useStore = create<State>()(
   persist(
@@ -38,56 +36,68 @@ export const useStore = create<State>()(
       addFinisher: (player) => {
         const { notFinishers, finishers, time, identifier } = get();
 
-        const newFinisher = {...player, time: time }
+        const newFinisher = { ...player, time: time };
 
-        set({ finishers: [...finishers, newFinisher].sort(sortByTime)})
-        set({ notFinishers: notFinishers.filter(pj => pj[identifier as keyof typeof player] !== player[identifier as keyof typeof player])})
+        set({ finishers: [...finishers, newFinisher].sort(sortByTime) });
+        set({
+          notFinishers: notFinishers.filter(
+            (pj) =>
+              pj[identifier as keyof typeof player] !==
+              player[identifier as keyof typeof player]
+          ),
+        });
       },
 
       setIdentifier: (id) => {
-        set({identifier: id})
+        set({ identifier: id });
       },
 
       setPlayers: (playersParameter) => {
-        set({players: playersParameter})
-        set({notFinishers: playersParameter})
-        set({finishers: []})
-        
+        set({ players: playersParameter });
+        set({ notFinishers: playersParameter });
+        set({ finishers: [] });
       },
       setTime: (newTime) => {
-        const { startTime } = get()
+        const { startTime } = get();
 
-        if(startTime == 0){
-          set({startTime: Date.now()})
+        if (startTime == 0) {
+          set({ startTime: Date.now() });
         }
-        if(newTime == 0){
-          set({startTime: 0})
+        if (newTime == 0) {
+          set({ startTime: 0 });
         }
-        
-        set({time: newTime})
+
+        set({ time: newTime });
       },
       setIsRunning: (bool) => {
-
-        set({isRunning: bool})
+        set({ isRunning: bool });
       },
       resetTable: () => {
         const { players } = get();
-        set({notFinishers: [...players]})
-        set({finishers: []})
+        set({ notFinishers: [...players] });
+        set({ finishers: [] });
       },
       deleteFinisher: (player) => {
-        const {notFinishers, finishers, identifier} = get();
+        const { notFinishers, finishers, identifier, players } = get();
 
         set({
-          finishers: finishers.filter(pj => pj[identifier as keyof typeof player] !== player[identifier as keyof typeof player]),
-             notFinishers: [...notFinishers, player].sort(sortByIdentifier(identifier))
-        })
-
-      }
-
+          finishers: finishers.filter(
+            (pj) =>
+              pj[identifier as keyof typeof player] !==
+              player[identifier as keyof typeof player]
+          ),
+        });
+        if ( players.some((pj) => pj[identifier] === player[identifier]) ) {
+          set({
+            notFinishers: [...notFinishers, player].sort(
+              sortByIdentifier(identifier)
+            ),
+          });
+        }
+      },
     }),
     {
-      name: 'marathon-app'
+      name: 'marathon-app',
     }
   )
-)
+);
