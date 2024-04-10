a33435b2cb11ddddc46f23a5aab791c6e3259c02
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-10 19:30:11 -0300

    fix identifier type bug

--- src/helpers/sortByIdentifier.ts
+++ src/helpers/sortByIdentifier.ts
@@ -1,8 +1,6 @@
+import { Player } from '../interfaces/interfaces';
 
-interface Player {
-  time: number;
-  [key: string]: any;
-}
-export const sortByIdentifier = (identifier:any) => {
-  return (a:{}, b:{}) => a[identifier as keyof typeof player] - b[identifier as keyof typeof player];
+
+export const sortByIdentifier = (identifier:string) => {
+  return (a:Player, b:Player) => a[identifier as keyof typeof a] - b[identifier as keyof typeof b];
 }
