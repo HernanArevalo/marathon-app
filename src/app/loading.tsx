def4beb28265d16101ee815bee77a5eee57f3728
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-11 18:56:50 -0300

    add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function LoadingPage() {
+  return (
+    <div className="w-[90vw] h-[90vh] bg-[color:var(--dark)] flex flex-col justify-center items-center">
+      <div className="loadership_XAGXB"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+  );
+}
