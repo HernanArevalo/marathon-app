c0fa71ae1ed29a22dd3a6cb83e58f597ecb73ed2
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-11 19:00:18 -0300

    Format document

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,20 @@ export default function HomePage() {
   return (
     <div className="flex flex-col justify-center items-center min-h-[100vh] md:flex-row w-full fade-in">
       <div
-        className={ clsx( players.length > 0 ? "md:w-[50%]":"md:w-[70%]",
+        className={clsx(
+          players.length > 0 ? 'md:w-[50%]' : 'md:w-[70%]',
           'relative w-full min-h-[100vh] flex flex-col justify-center items-center p-5 gap-10'
-        )
-        }
+        )}
       >
         <div className="absolute top-10 left-10 flex gap-3">
           <ChargeTableButton />
           <DeleteTableButton />
         </div>
         <Counter />
-          <FinisherForm />
+        <FinisherForm />
       </div>
 
-          <ResultsList />
+      <ResultsList />
     </div>
   );
 }
