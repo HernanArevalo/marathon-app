e4ff8120026f5d8a6ec2960c01d3eb9b34a5ca01
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-17 23:47:35 -0300

    format document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ const inter = Kanit({ subsets: ['latin'], weight: ['400'] });
 
 export const metadata: Metadata = {
   title: 'Marathon | Race',
-  description: 'Aplicación para controlar y registrar los tiempos de los participantes en una carrera. Ingresando los datos de éstos desde un archivo excel, puedes ir ingresando su tiempo a medida que atraviesan la meta. Luego puedes guardar la tabla de tiempos en un nuevo archivo excel.',
+  description:
+    'Aplicación para controlar y registrar los tiempos de los participantes en una carrera. Ingresando los datos de éstos desde un archivo excel, puedes ir ingresando su tiempo a medida que atraviesan la meta. Luego puedes guardar la tabla de tiempos en un nuevo archivo excel.',
 };
 
 export default function RootLayout({
@@ -14,13 +15,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-  const project_Version = process.env.npm_package_version
+  const project_Version = process.env.npm_package_version;
 
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={`${inter.className} flex justify-center items-center`}>
-        <span className="absolute text-gray-700 bottom-1 right-1">v. {project_Version}</span>
+        <span className="absolute text-gray-700 bottom-1 right-1">
+          v. {project_Version}
+        </span>
         {children}
       </body>
     </html>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,3 @@
-
 export default function LoadingPage() {
   return (
     <div className="w-[90vw] h-[90vh] bg-[color:var(--dark)] flex flex-col justify-center items-center">
